77802a8be11804d812887a58d272f79db982445f
blosomaj <jselizabeth96@gmail.com>
2018-04-06 10:59:01 +0530

    Update Automatic Malaria Detection.m

--- Automatic Malaria Detection.m
+++ Automatic Malaria Detection.m
@@ -4,3 +4,9 @@ clc
 load('normdata.mat');
 load('maldata.mat');
 [filename,pathname]=uigetfile('*jpg');
+I=imread([pathname,filename]);
+figure,imshow(I);
+
+red=I(:,:,1);red=imadjust(red);figure,imshow(red);
+green=I(:,:,1);green=imadjust(green);figure,imshow(green);
+blue=I(:,:,1);blue=imadjust(blue);figure,imshow(blue);
